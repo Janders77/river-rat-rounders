0f8e3ab0271748c8705825454729d33e025c8d7d
anders.jay <anders.jay@outlook.com>
2026-02-24 21:39:06 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import { createPageUrl } from "@/utils";
-import { Trophy, Plus, History, Menu, Home } from "lucide-react";
+import { Trophy, Plus, History, Menu, Home, ShieldAlert } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
