0f791281f907928c3e6d73e07e56e0a65ab9777e
anders.jay <anders.jay@outlook.com>
2026-02-24 22:05:19 +0000

    File changes

--- src/pages/DirectorDashboard.jsx
+++ src/pages/DirectorDashboard.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { base44 } from "@/api/base44Client";
 import { Game } from "@/entities/Game";
 import { User } from "@/entities/User";
+import { GameSession } from "@/entities/GameSession";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
