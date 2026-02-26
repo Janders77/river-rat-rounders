01fda2f351839227bf2df10dc588b452274d493a
Jay Anders <anders.jay@outlook.com>
2026-02-26 23:38:24 +0000

    File changes

--- functions/deleteDuplicatePlayers.ts
+++ functions/deleteDuplicatePlayers.ts
@@ -39,6 +39,7 @@ Deno.serve(async (req) => {
         for (let i = 1; i < players.length; i++) {
           await base44.asServiceRole.entities.Player.delete(players[i].id);
           deletedCount++;
+          await new Promise(resolve => setTimeout(resolve, 50));
         }
       }
     }
