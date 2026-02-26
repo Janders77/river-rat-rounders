59f031b9b08fa75f32a4d7aa232af131f44db62d
Jay Anders <anders.jay@outlook.com>
2026-02-26 21:31:08 +0000

    File changes

--- src/pages/PlayerDatabase.jsx
+++ src/pages/PlayerDatabase.jsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { UserPlus, Upload, Users, Search } from "lucide-react";
+import { createPageUrl } from "@/utils";
 import PlayerFilters from "../components/PlayerFilters";
 import PlayerRow from "../components/PlayerRow";
 
