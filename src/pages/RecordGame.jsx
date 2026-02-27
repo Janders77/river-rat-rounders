c16596ffc93d4b9730163a9a1844140efb455ae9
Jay Anders <anders.jay@outlook.com>
2026-02-27 18:16:42 +0000

    File changes

--- src/pages/RecordGame.jsx
+++ src/pages/RecordGame.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { base44 } from "@/api/base44Client";
 import { Game } from "@/entities/Game";
 import { User } from "@/entities/User";
 import { Button } from "@/components/ui/button";
