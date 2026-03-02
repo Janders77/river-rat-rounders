02162bf89e1211c7124c0500637a9f31248c88fa
Jay Anders <anders.jay@outlook.com>
2026-03-02 20:40:24 +0000

    File changes

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -441,7 +441,7 @@ const sidebarMenuButtonVariants = cva(
   }
 )
 
-const SidebarMenuButton = React.forwardRef(({
+const SidebarMenuButton = React.forwardRef(function SidebarMenuButtonInner({
     asChild = false,
     isActive = false,
     variant = "default",
@@ -451,7 +451,7 @@ const SidebarMenuButton = React.forwardRef(({
     ...props
   },
   ref
-) => {
+) {
   const Comp = asChild ? Slot : "button"
   const { isMobile, state } = useSidebar()
 
