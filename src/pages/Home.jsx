61b457b81d200ecce3f6043080bccfa07b05160b
Jay Anders <anders.jay@outlook.com>
2026-03-02 21:28:33 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ import PlayerSignIn from "../components/home/PlayerSignIn";
 import PlayerSignUp from "../components/home/PlayerSignUp";
 import LoginCard from "../components/home/LoginCard";
 
-const cardClass = "flex items-center gap-4 p-5 rounded-xl border border-red-900/40 bg-gradient-to-r from-red-900/20 to-red-950/60 hover:border-red-800/60 hover:from-red-900/30 hover:to-red-950/80 transition-all duration-200 group";
+const cardClass = "flex items-center gap-4 p-5 rounded-xl border border-red-900/40 bg-gradient-to-r from-red-900/20 to-red-950/60 hover:border-red-700/70 hover:from-red-700/40 hover:to-red-800/50 hover:bg-red-800/30 transition-all duration-200 group";
 
 const navLinks = [
   {
