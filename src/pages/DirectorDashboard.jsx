3f8e3e0ef1490d34730aa1e3c7c6d5de90c55f7f
Jay Anders <anders.jay@outlook.com>
2026-03-05 17:24:29 +0000

    File changes

--- src/pages/DirectorDashboard.jsx
+++ src/pages/DirectorDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { base44 } from "@/api/base44Client";
 import { Game } from "@/entities/Game";
 import { User } from "@/entities/User";
